28eb8a28ac23627b23712c0b3baa2ce4edac600e
Mirco Bisoffi <bismirco@gmail.com>
2026-01-24 11:31:58 +0100

    add Dockerfile and requirments.txt

--- inventory-service/app/database.py
+++ inventory-service/app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from typing import Optional
 import os 
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
 connect_args = {"check_same_thread": False}
 engine = create_engine(DATABASE_URL, connect_args=connect_args)
