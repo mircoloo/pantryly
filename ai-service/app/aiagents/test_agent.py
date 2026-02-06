97c895780c2954156f5438a0cf6a444cd6626b5b
Mirco <bismirco@gmail.com>
2026-02-06 22:17:21 +0100

    modified api to use gemini

--- ai-service/app/aiagents/test_agent.py
+++ ai-service/app/aiagents/test_agent.py
@@ -14,55 +14,51 @@ gemini = OpenAI(api_key=google_api_key, base_url="https://generativelanguage.goo
 
 
 
+# chat_agent = Agent(name="Chef Assistant", 
+#                    instructions="You are a helpful assistant which receives a list of products from the pantry and have to suggest  \
+#                    some receipe to the user, your language is Italian you must answer in this language. Use ONLY the products provided \
+#                        Do not ask to the user if has other ingredients. just use the one in the provided,. Furthermore ask product based on the expiration date\
+#                            so if they are almost to the deadline use those first ingredient. Devi ritornare gli ingredienti da utilizzare, e la ricetta. Inoltre il nome della ricetta e il tipo di piatto (se dolce, primo piatto etc \
+#                             Puoi ritornare anche una lista di ricette se ti va in modo da dare più idee se le hai", 
+#                    model="gpt-4o-mini",
+#                    output_type=List[schemas.Receipe]
+#                    )
 
-chat_agent = Agent(name="Chef Assistant", 
-                   instructions="You are a helpful assistant which receives a list of products from the pantry and have to suggest  \
-                   some receipe to the user, your language is Italian you must answer in this language. Use ONLY the products provided \
-                       Do not ask to the user if has other ingredients. just use the one in the provided,. Furthermore ask product based on the expiration date\
-                           so if they are almost to the deadline use those first ingredient. Devi ritornare gli ingredienti da utilizzare, e la ricetta. Inoltre il nome della ricetta e il tipo di piatto (se dolce, primo piatto etc \
-                            Puoi ritornare anche una lista di ricette se ti va in modo da dare più idee se le hai", 
-                   model="gpt-4o-mini",
-                   output_type=List[schemas.Receipe]
-                   )
-
-categorization_product = Agent(name="Categorizzatore di prodotti", 
-                   instructions="Sei un agente che deve categorizzare i vari prodotti in modo che siano ben organizzati da mostrare all'utente in maniera chiara. \
-                       Per esempio prodotti per la colazione, per il bagno, per i primi piatti etc", 
-                   model="gpt-4o-mini",
-                   output_type=List[schemas.CategorizedProduct]
-                   )
+# categorization_product = Agent(name="Categorizzatore di prodotti", 
+#                    instructions="Sei un agente che deve categorizzare i vari prodotti in modo che siano ben organizzati da mostrare all'utente in maniera chiara. \
+#                        Per esempio prodotti per la colazione, per il bagno, per i primi piatti etc", 
+#                    model="gpt-4o-mini",
+#                    output_type=List[schemas.CategorizedProduct]
+#                    )
 
 
 async def process_get_categorized_products(products: List[schemas.Product]):
     query: str = f"Given the ingredients given here: {json.dumps(products, indent=2)} give a category at each product and return the same but with the category"
-    result = await Runner.run(categorization_product, 
-                              input=[
-                                        {
-                                            "role": "user",
-                                            "content": query
-                                        }
-                                    ]
-                            )
+    messages = [{"role": "user", "content": query}]
+    
+    response = gemini.chat.completions.parse(
+        model="gemini-3-flash-preview",
+        messages=messages,
+        response_format=schemas.CategorizedProductsResponse
+    )
+    # If using response_format, the returned object is already your model
+    return response.choices[0].message.parsed
     return result.final_output
 
 async def process_request(products : List[schemas.Product]):
-    
     query: str = f"Given the ingredients given here: {json.dumps(products, indent=2)} suggest a nice and delicious receipe, is not necessary to use all the ingredients, the receipe has to have some sense. Respond in language Italian."
-    
-    result = await Runner.run(chat_agent, 
-                              input=[
-                                        {
-                                            "role": "user",
-                                            "content": query
-                                        }
-                                    ]
-                            )
-    return result.final_output
+    messages = [{"role": "user", "content": query}]
+    response = gemini.chat.completions.parse(
+        model="gemini-3-flash-preview",
+        messages=messages,
+        response_format=schemas.ReceipesResponse
+    )
+    # If using response_format, the returned object is already your model
+    return response.choices[0].message.parsed
 
 async def get_response_chat(request_text: str):
     print(request_text)
     messages = [{"role": "user", "content": request_text}]
-    
     response = gemini.chat.completions.parse(
         model="gemini-3-flash-preview",
         messages=messages,
--- ai-service/app/schemas.py
+++ ai-service/app/schemas.py
@@ -1,24 +1,34 @@
 from pydantic import BaseModel, Field
 from datetime import date
 from typing import List
+
+
+class Product(BaseModel):
+    name: str
+    expiration_date: date
+    barcode: str = Field(description="Il codice a barre del prodotto")
 class Receipe(BaseModel):
-    name: str = Field(description="The name of the receipe")
-    dish_type: str = Field(description="Type of dish, if is a dessert or a first course etc")
-    ingredients: List[str] = Field(description="A list of ingredients for the given receipe")
+    name: str = Field(description="Il nome della ricetta")
+    dish_type: str = Field(description="Il tipo di piatto, se primo piatto dolce o altro")
+    ingredients: List[Product] = Field(description="La lista degli ingredienti per la ricetta")
     receipe: str = Field(description="The various step to complete the receipe")
-
-
-
+    
+class ReceipesResponse(BaseModel):
+    receipes: List[Receipe]
 
 class AIChatRequest(BaseModel):
     request: str
     
 class AIChatResponse(BaseModel):
     response: str
+
     
-class Product(BaseModel):
-    name: str
-    expiration_date: date
+
     
 class CategorizedProduct(Product):
     category: str
+
+class CategorizedProductsResponse(BaseModel):
+    products: List[CategorizedProduct]
+    
+
