21ddd9b85f681f86ef30621f6839b82275756894
Mirco <bismirco@gmail.com>
2026-04-13 14:44:00 +0200

    added ProductBase schema to have a cleaner code

--- microservices/inventory-service/app/schemas/schemas.py
+++ microservices/inventory-service/app/schemas/schemas.py
@@ -3,19 +3,18 @@ from typing import Optional
 
 from pydantic import BaseModel, ConfigDict
 
-
-class ProductCreate(BaseModel):
+class ProductBase(BaseModel):
     name: str
-    user_id: int
     barcode: str
     expiration_date: Optional[date] = None
 
+class ProductCreate(ProductBase):
+    user_id: int
 
-class ProductShow(BaseModel):
+
+
+class ProductShow(ProductBase):
     id: int
     user_id: int
-    name: str
-    barcode: str
-    expiration_date: Optional[date] = None
 
     model_config = ConfigDict(from_attributes=True)
